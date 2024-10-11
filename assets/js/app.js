622c22580d5419d89469cc165b61ba82cbfb4479
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2024-10-11 08:35:52 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,3 @@
-// window.addEventListener('load', () => {
-//   document.body.classList.remove("clean__transition");
-// })
+window.addEventListener("load", () => {
+  document.body.classList.remove("clean__transition");
+});
